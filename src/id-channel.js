a39c7c6d34542433b0addbb7ad762bdb0285a7ab
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-12 21:25:50 -0500

    Actualizar src

---
+++ src/id-channel.js
@@ -0,0 +1,24 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async msg => {
+    if (msg.body === '!channelid') {
+        const chat = await msg.getChat();
+        const channelId = chat.id._serialized;
+
+        msg.reply(`La ID de este canal es: ${channelId}`);
+    }
+});
+
+client.initialize();
